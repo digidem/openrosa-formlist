3ff522e7bd18645f0e1caa2f4a038c91e67facfc
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-26 10:03:37 -0700

    Add tests for streams

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var test = require('tape')
 var fs = require('fs')
+var resumer = require('resumer')
+var request = require('request')
 var createFormList = require('../')
 
 var forms = [
@@ -10,7 +12,7 @@ var forms = [
 ]
 
 test('Produces correct formList API', function (t) {
-  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist.xml').toString().replace(/\n$/, '')
+  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist.xml').toString().trim()
 
   createFormList(forms, function (err, result) {
     t.error(err, 'Does not produce error')
@@ -20,7 +22,7 @@ test('Produces correct formList API', function (t) {
 })
 
 test('Can pass custom downloadUrl template', function (t) {
-  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist-download-url.xml').toString().replace(/\n$/, '')
+  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist-download-url.xml').toString().trim()
 
   var options = {
     downloadUrl: 'https://example.com/formXml?formId=${formId}'
@@ -34,13 +36,45 @@ test('Can pass custom downloadUrl template', function (t) {
 })
 
 test('Adds a manifestUrl tag when xForm references external media and option is set', function (t) {
-  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist-manifest-url.xml').toString().replace(/\n$/, '')
+  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist-manifest-url.xml').toString().trim()
 
   var options = {
     manifestUrl: 'https://example.com/xformsManifest?formId=${formId}'
   }
 
   createFormList(forms, options, function (err, result) {
+    t.error(err, 'Does not produce error')
+    t.equal(result, expectedXml, 'Matches expected xml FormList')
+    t.end()
+  })
+})
+
+test('Accepts an array of request streams', function (t) {
+  var formStreams = forms.map(function (url) {
+    return request(url)
+  })
+  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist.xml').toString().trim()
+
+  createFormList(formStreams, function (err, result) {
+    t.error(err, 'Does not produce error')
+    t.equal(result, expectedXml, 'Matches expected xml FormList')
+    t.end()
+  })
+})
+
+test('Accepts an array of streams from strings', function (t) {
+  var formString = fs.readFileSync(__dirname + '/fixtures/birds.xml').toString()
+  var formStream = resumer().queue(formString).end()
+  formStream.uri = {
+    href: 'https://raw.githubusercontent.com/digidem-test/xform-test/master/forms/birds/form.xml'
+  }
+  var formStreams = [
+    formStream
+  ]
+
+  var expectedXml = fs.readFileSync(__dirname + '/fixtures/formlist-birds.xml').toString().trim()
+
+  createFormList(formStreams, function (err, result) {
     t.error(err, 'Does not produce error')
     t.equal(result, expectedXml, 'Matches expected xml FormList')
     t.end()
